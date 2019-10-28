586b52af27fb1e9840d99dc66c84b0c8b5ae1cdc
marysinaida <marydorcassinaida54@gmail.com>
2019-10-28 11:59:03 +0300

    change description to recite at quote.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,12 @@ import {Quote} from './quote';
 export class AppComponent {
   title = 'quote';
   quotes:Quote [] =[
-    {id:1,name:'"Change the world by being yourself"',description:'Author,upvote,downvote'},
-    {id:2,name:'"I don’t need it to be easy, I need it to be worth it. "',description:'Author,upvote,downvote'},
-    {id:3,name:'"Reality is wrong, dreams are for real. "',description:'Author,upvote,downvote'},
-    {id:4,name:'"May your choices reflect your hopes, not your fears."',description:'Author,upvote,downvote'},
-    {id:5,name:'"Turn your wounds into wisdom. "',description:'Author,upvote,downvote'},
-    {id:6,name:'"Happiness depends upon ourselves. "',description:'Author,upvote,downvote'},
+    {id:1,name:'"Change the world by being yourself"',recite:'Author,upvote,downvote'},
+    {id:2,name:'"I don’t need it to be easy, I need it to be worth it. "',recite:'Author,upvote,downvote'},
+    {id:3,name:'"Reality is wrong, dreams are for real. "',recite:'Author,upvote,downvote'},
+    {id:4,name:'"May your choices reflect your hopes, not your fears."',recite:'Author,upvote,downvote'},
+    {id:5,name:'"Turn your wounds into wisdom. "',recite:'Author,upvote,downvote'},
+    {id:6,name:'"Happiness depends upon ourselves. "',recite:'Author,upvote,downvote'},
   ];
   
   }
